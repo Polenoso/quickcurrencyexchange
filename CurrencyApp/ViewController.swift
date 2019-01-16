426d7bd7bea54f65bf8834792fcef26f1e5c72c6
Aitor Pagán <aitor.pagan@cornerjob.com>
2019-01-16 11:22:24 +0100

    Initial Commit

---
+++ CurrencyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyApp
+//
+//  Created by Aitor Pagán on 16/1/19.
+//  Copyright © 2019 polenoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
